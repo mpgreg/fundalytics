c4ea42645532df52b98d7fc1d83c44c30174f5bc
mpgreg <michaelgregory122@gmail.com>
2024-05-30 13:55:41 +0200

    streamlit data viewer

--- streamlit/fundalytics_app.py
+++ streamlit/fundalytics_app.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from pathlib import Path
 from PIL import Image
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid.shared import JsCode
 from time import sleep
 from textwrap import dedent
 from funda_scraper import FundaScraper
@@ -49,16 +51,6 @@ if "city_list" not in st.session_state:
 else:
     city_list = st.session_state["city_list"]
 
-if "min_size" not in st.session_state:
-    min_size = 0
-else:
-    min_size = st.session_state["min_size"]
-
-if "max_size" not in st.session_state:
-    max_size = None
-else:
-    max_size = st.session_state["max_size"]
-
 def scrape_and_process_data(scraper: FundaScraper) -> pd.DataFrame:
     
         df = scraper.run(raw_data=False, save=False)
@@ -96,7 +88,6 @@ def import_data(ingest_df: pd.DataFrame):
         print("error")
 
 
-
 header_image = Image.open(Path(__file__).parent / "logo.png")
     
 st.markdown(
@@ -126,15 +117,21 @@ with st.container():
 with st.sidebar:
 
     city_name = st.selectbox(
-            label="Select a city name.",
+            label="Select a city name. **",
             index=None,
             options=city_list,
             )
     
     want_to = st.selectbox(
-            label="Select a transaction type.",
+            label="Select a transaction type. **",
             index=None,
             options=["buy", "rent"],
+            )
+    
+    property_type = st.selectbox(
+            label="Select a property type. **",
+            index=None,
+            options=["house", "apartment"],
             )
     
     min_price = st.number_input(
@@ -156,12 +153,17 @@ with st.sidebar:
             options=[None, 1, 3, 5, 10, 30],
             ) 
     
-    if city_name and want_to:
+    st.write("** denotes mandatory fields.")
+    
+    import_df = None
+
+    if city_name and want_to and property_type:
         if st.button(label="Import Data"):
             
             scraper = FundaScraper(
                 area=city_name, 
                 want_to=want_to, 
+                property_type=property_type,
                 days_since=days_since,
                 min_price=min_price,
                 max_price=max_price,
@@ -177,185 +179,36 @@ with st.sidebar:
 
             import_data(import_df)
 
-
 # collection.query.fetch_objects(filters=Filter.by_property("city").equal("hoorn"))
 
-# ingest_tab, finsum_tab, finsum_qna = st.tabs(
-#     ["Ingest New Ticker", "FinSum 10-Q Summarization", "FinSum Q&A"]
-# )
+dataviewer_tab, threedviewer_tab, image_search_tab = st.tabs(
+    ["Data Viewer", "3D Viewer", "Image Search"]
+)
 
-# with finsum_tab:
-#     st.header("FinSum 10-Q Summarization")
+with dataviewer_tab:
+    st.header("Data Viewer")
         
-#     if not fp:
-#         st.write("⚠️ Select a company, fiscal year and fiscal period in the side bar.")
-#     else:
-#         st.write(f"Summary for {selected_company['title']} in fiscal period FY{fy}{fp}.")
+    if import_df is None:
+        st.write("⚠️ Select at least a city and transaction type in the side bar.")
+    else:
+        st.write(f"Summary for {property_type} to {want_to} in {city_name}.")
 
-#         summary = (
-#             weaviate_client.query.get(summary_class, ["summary"])
-#                 .with_where({
-#                     "operator": "And",
-#                     "operands": [
-#                     {
-#                         "path": ["tickerSymbol"],
-#                         "operator": "Equal",
-#                         "valueText": selected_company["ticker"]
-#                     },
-#                     {
-#                         "path": ["fiscalYear"],
-#                         "operator": "Equal",
-#                         "valueInt": fy
-#                     },
-#                     {
-#                         "path": ["fiscalPeriod"],
-#                         "operator": "Equal",
-#                         "valueText": fp
-#                     }
-#                 ]
-#             })
-#             .do()
-#         )["data"]["Get"][summary_class][0]["summary"]
+        display_df = import_df.drop(['image_enc','descrip', 'uuid'], axis=1)
+        
+        display_df[''] = display_df.apply(
+            lambda x: '<a href="{house_url}"><img src="{image_url}" width="60" ></a>'.format(
+                image_url=x.image_url,
+                house_url=x.url),
+            axis=1
+            )
+        
+        display_df.set_index('', inplace=True)
 
-#         st.markdown(summary)
-
-# with finsum_qna:
-#     st.write(dedent("""
-#         Ask a question regarding financial statements for the chosen company.  
-#         FinSum will vectorize the question, retrieve related documents from 
-#         the vector database and use that as context for OpenAI to generate 
-#         a response."""))
-    
-#     if not selected_company:
-#         st.write("⚠️ Select a company in the side bar.")
-#         question = None
-#     else:
-#         question = st.text_area("Question:", placeholder="")
-
-#     if question:
-#         ask = {
-#             "question": question,
-#             "properties": ["content", "tickerSymbol", "fiscalYear", "fiscalPeriod"],
-#             # "certainty": 0.0
-#         }
-
-#         st.write("Showing search results for:  " + question)
-#         st.subheader("10-Q results")
-#         results = (
-#             weaviate_client.query.get(
-#                 chunk_class, 
-#                 ["docLink", 
-#                     "tickerSymbol", 
-#                     "_additional {answer {hasAnswer property result} }"
-#                 ])
-#             .with_where({
-#                 "path": ["tickerSymbol"],
-#                 "operator": "Equal",
-#                 "valueText": selected_company["ticker"]
-#             })
-#             .with_ask(ask)
-#             .with_limit(3)
-#             .with_additional(["certainty", "id", "distance"])
-#             .do()
-#         )
-
-#         if results.get("errors"):
-#             for error in results["errors"]:
-#                 if ("no api key found" or "remote client vectorize: failed with status: 401 error") in error["message"]:
-#                     raise Exception("Cannot vectorize.  Check the OpenAI key in the airflow connection.")
-#                 else:
-#                     st.write(error["message"])
-
-#         elif len(results["data"]["Get"][chunk_class]) > 0:
-#             docLinks = []
-#             link_count = 1
-#             for result in results["data"]["Get"][chunk_class]:
-#                 if result["_additional"]["answer"]["hasAnswer"]:
-#                     write_response(result["_additional"]["answer"]["result"])
-#                 docLinks.append(f"[{link_count}]({result['docLink']})")
-#                 link_count = link_count + 1
-#             st.write(",".join(docLinks))
-#             # st.markdown(disclaimer, unsafe_allow_html=True)
-
-# with ingest_tab:
-#     st.header("Ingest new financial data")
-
-#     st.write("""By selecting a company from the list below an Airflow DAG run will be 
-#              triggered to extract, embed and summarize financial statements. Search 
-#              by company name, ticker symbol or CIK number.""")
-    
-#     company_to_ingest = st.selectbox(
-#         label="Select a company.",
-#         index=None,
-#         options=company_list,
-#         format_func=format_tickers
-#         )
-    
-#     if company_to_ingest: 
-
-#         if st.button(label="Start Ingest"):
-
-#             response = requests.post(
-#                 url=f"{webserver_internal}/api/v1/dags/{dag_id}/dagRuns",
-#                 headers={"Content-Type": "application/json"},
-#                 auth=requests.auth.HTTPBasicAuth(
-#                     webserver_username, webserver_password),
-#                 data=json.dumps({
-#                     "conf":  {
-#                         "run_date": str(datetime.now()), 
-#                         "ticker": company_to_ingest["ticker"]
-#                         }
-#                     })
-#                 )
-            
-#             if response.ok:
-#                 run_id = json.loads(response.text)['dag_run_id']
-#                 link = f"{webserver_public}/dags/{dag_id}/grid?dag_run_id={run_id}&tab=graph"
-#                 status_link = f"{webserver_internal}/api/v1/dags/{dag_id}/dagRuns/{run_id}"
-                    
-#                 status = requests.get(
-#                     url=status_link,
-#                     headers={"Content-Type": "application/json"},
-#                     auth=requests.auth.HTTPBasicAuth(
-#                         webserver_username, webserver_password),
-#                     )
-
-#                 if status.ok:
-#                     state = json.loads(status.content).get("state")
-
-#                     if state in ["running", "queued"]:
-#                         st.markdown(dedent(f"""
-#                             Document ingest runnging for ticker {company_to_ingest["ticker"]}. \n
-#                             Check status in the [Airflow webserver]({link})"""))
-#                         st.write("⚠️ Do not refresh your browser.")
-#                     else:
-#                         st.error(f"Ingest not running: {state}")
-                    
-#                     with st.spinner():
-            
-#                         while state in ["running", "queued"]:
-#                             sleep(5)
-
-#                             status = requests.get(url=status_link,
-#                                 headers={"Content-Type": "application/json"},
-#                                 auth=requests.auth.HTTPBasicAuth(
-#                                     webserver_username, webserver_password),
-#                                 )
-                            
-#                             if status.ok:
-#                                 state = json.loads(status.content).get("state")
-#                             else:
-#                                 st.error(status.reason)
-                        
-#                     if state == "success":
-#                         st.success(dedent(f"""
-#                             Ingest complete for ticker {company_to_ingest['ticker']}. 
-#                             Please refresh your browser."""))
-#                     else:
-#                         st.error(f"Ingest failed: state {state}")
-                
-#                 else:
-#                     st.error(f"Ingest failed: state {status.reason}")
-                    
-#             else:
-#                 st.error(f"Could not start DAG: {response.reason}")
+        display_columns=['address', 'living_area', 'price', 'price_m2', 'bedroom', 'bathroom', 'energy_label']
+        pd.options.display.html.border = 0
+        st.markdown(
+            display_df[display_columns].to_html(
+                escape=False,
+                border=0).replace('<table class=', '<table border="0" class='), 
+            unsafe_allow_html=True)
+        
